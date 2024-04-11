c46e4feb2f31f320a9110eb74b0925b8e7bc943a
AngelFoxJP <Japaroski@gmail.com>
2024-04-11 11:29:25 -0500

    Added file type verification to ensure that only image files are accepted

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,20 +1,32 @@
+const fs = require('fs');
 
 const uploadPFP = async (req, res) => {
-    console.log("Profile Image Upload Started")
-    if (!req?.user) return res.status(500).json({"message": "Error! Non-authed user. No user field found when attempting to upload pfp"})
-    
-    var file;
-    if(!req.files)
-        return res.status(400).send("No files were uploaded!");
-    
-    file = req.files.pfp;
+    var file = req.files.pfp;
+    allowedTypes = [ "image/jpeg", "image/png", "image/webp"];
     uploadPath = __dirname + '/../public/img/pfp/' + req.user + '.png';
-    console.log(uploadPath);
+    
+    if (!req?.user) return res.status(500).json({"message": "Error! Non-authed user. No user field found when attempting to upload pfp"})
+    if (!req.files) return res.status(400).send("No files were uploaded!");
+    console.log(file.mimetype);
+    if (!allowedTypes.includes(file.mimetype)) return res.status(400).send("Filetype not supported");
+
     file.mv(uploadPath, function(err) {
-        if (err)
-            return res.status(500).send(err)
+        if (err) {
+            res.status(500);
+            error(err);
+        }
         res.send("File Uploaded");
     })
 }
 
-module.exports = { uploadPFP };
+const getImage = async (req, res) => {
+    const cookies = req.cookies;
+    if (!cookies?.jwt) return res.sendStatus(401);
+    profilePath = path.join(__dirname, '..', '..', 'public', 'img', 'pfp', cookies.UserName + '.png');
+    if (fs.existsSync(profilePath))
+        res.sendFile(profilePath);
+    else
+        res.sendFile(path.join(__dirname, '..', '..', 'public', 'img', 'default.png'));
+}
+
+module.exports = { uploadPFP, getImage };
