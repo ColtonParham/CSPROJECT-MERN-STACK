f1033c15069c83af9fe5d302c9e12177693cbad3
AngelFoxJP <Japaroski@gmail.com>
2024-04-10 23:25:50 -0500

    Updated main pfp to automatically choose the user's pfp\n Also added username to maxCalc.html

--- routes/pfp.js
+++ routes/pfp.js
@@ -6,6 +6,16 @@ const path = require('path');
 const logoutController = require('../controllers/logoutController');
 const fs = require('fs');
 
+router.get('/', (req, res) => {
+    const cookies = req.cookies;
+    if (!cookies?.jwt) return res.sendStatus(401);
+    profilePath = path.join(__dirname, '..', 'public', 'img', 'pfp', cookies.UserID + '.png');
+    if (fs.existsSync(profilePath))
+        res.sendFile(profilePath);
+    else
+        res.sendFile(path.join(__dirname, '..', 'public', 'img', 'default.png'));
+})
+
 router.get('/:id', (req, res) => {
     profilePath = path.join(__dirname, '..', 'public', 'img', 'pfp', req.params.id + '.png');
     if (fs.existsSync(profilePath))
