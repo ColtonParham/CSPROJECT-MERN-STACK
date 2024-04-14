d874a366fe16d9854ddf5769dfe9988d3dbbf61b
AngelFoxJP <Japaroski@gmail.com>
2024-04-14 02:30:50 -0500

    Created api.js for easier coding; Updated some files
    Includes multiple new functions such as xmlRequest(Method, Path, Data,
    Auth).
    Best to call some of these functions using Async.
    To see more info on any function, refer to the comments above them
    or simply ask me about them

---
+++ public/js/api.js
@@ -0,0 +1,66 @@
+var authToken;
+function getCookie(name) {
+    return (name = (document.cookie + ';').match(new RegExp(name + '=.*;'))) && name[0].split(/=|;/)[1];
+}
+
+// Authenticates the user if not authenticated.
+async function auth() {
+    if (isAuth()) return true;
+    console.log("Authenticating")
+    try {
+        authToken = JSON.parse(await xmlRequest("GET", "refresh")).accessToken;
+        console.log("Authenticated")
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+// Returns if user is currently authenticated
+function isAuth() {
+    return !(authToken == null)
+}
+
+// Returns the authToken
+async function getAuth() {
+    if (!isAuth())
+        await auth();
+    return authToken;
+}
+
+// Returns a promise that resolves on a successful xml message, while rejecting when any failure occurs
+// Will get authToken for the user if needed
+async function xmlRequest(method, path, data = null, auth = false) {
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials=true;
+    xhr.open(method, path);
+    if (auth)
+        xhr.setRequestHeader('Authorization', 'Bearer ' + await getAuth());
+    return new Promise((resolve, reject) => {
+        // Set promise response for when the request is finished
+        xhr.onload = function(){
+            if (this.status >=200 && this.status < 300) // Check if any errors have occured.
+                resolve(xhr.response);
+            else {
+                reject({
+                    "status" : this.status,
+                    "statusText" : this.statusText,
+                    "response" : this.response,
+                });
+            }
+        }
+
+        // Deal with xhr errors
+        xhr.onerror = function () {
+            reject({
+                "status" : this.status,
+                "statusText" : this.statusText
+            });
+        }
+
+        // Send the request
+        if (data == null)
+            xhr.send();
+        else
+            xhr.send(data);
+    });
+}
