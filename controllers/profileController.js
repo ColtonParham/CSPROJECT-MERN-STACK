75e73d8d3784ddade86e802ba74b14801dc5def7
AngelFoxJP <Japaroski@gmail.com>
2024-04-15 01:59:31 -0500

    Fixed 400 error response that did not involve json

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
+// file deepcode ignore NoRateLimitingForExpensiveWebOperation: Vulurn ignored due to rate limiting placed before accessing controller.
 const uploadPFP = async (req, res) => {
     var file = req.files.pfp;
     allowedTypes = [ "image/jpeg", "image/png", "image/webp"];
@@ -9,7 +10,7 @@ const uploadPFP = async (req, res) => {
     if (!req?.user) return res.status(500).json({"message": "Error! Non-authed user. No user field found when attempting to upload pfp"})
     if (!req.files) return res.status(400).send("No files were uploaded!");
     console.log(file.mimetype);
-    if (!allowedTypes.includes(file.mimetype)) return res.status(400).send("Filetype not supported");
+    if (!allowedTypes.includes(file.mimetype)) return res.status(400).json({ "Message": "Filetype not supported"});
 
     file.mv(uploadPath, function(err) {
         if (err) {
@@ -25,7 +26,7 @@ const getImage = async (req, res) => {
     if (!req.params?.id) req.params.id = req.cookies.UserName;
     console.log(req.params.id);
     const format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
-    if (format.test(req.params.id)) return res.status(400).json({ "Message" : "Special characters are not allowed"});
+    if (format.test(req.params.id)) return res.status(400).json({ "Message": "Special characters are not allowed"});
     profilePath = path.join(__dirname, '..', 'public', 'img', 'pfp', req.params.id + '.png');
     if (fs.existsSync(profilePath))
         res.sendFile(profilePath);
